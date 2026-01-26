6b16298f152e4e9a756f7296bab3f6dc98b08afe
Sem20071 <sem2007160@gmail.com>
2026-01-26 10:52:49 +0700

    внесение изменений

--- 2_main/providers.tf
+++ 2_main/providers.tf
@@ -16,9 +16,9 @@ terraform {
     key = "terraform.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
+    skip_requesting_account_id  = true   
     skip_s3_checksum            = true
-    skip_metadata_api_check     = true
+    use_path_style              = true
     # dynamodb_table = "tfstate-lock"
   }
 }
