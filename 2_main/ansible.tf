9b29fd03b297a512c700107d4bca807cdb1bd23a
Sem20071 <sem2007160@gmail.com>
2026-01-26 13:38:07 +0700

    Внесение изменений

--- 2_main/ansible.tf
+++ 2_main/ansible.tf
@@ -16,11 +16,21 @@ resource "null_resource" "ansible_provisioner" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 60 && ansible-playbook -i ${path.module}/ansible-config-k8s-cluster/hosts.ini ${path.module}/ansible-config-k8s-cluster/k8s-cluster-config.yaml -k -u ansible-user --extra-vars 'ansible_ssh_pass=${var.root_user_pass} ansible_become_pass=${var.root_user_pass}'"
+    command = <<EOT
+      sleep 60 && \
+      ansible-playbook \
+      -i ${path.module}/ansible-config-k8s-cluster/hosts.ini \
+      ${path.module}/ansible-config-k8s-cluster/k8s-cluster-config.yaml \
+      -u ansible-user \
+      -k \
+      --extra-vars "ansible_ssh_pass='${var.root_user_pass}' ansible_become_pass='${var.root_user_pass}'" \
+      -e 'ansible_ssh_common_args="-o PreferredAuthentications=password -o PubkeyAuthentication=no"'
+    EOT
     
     environment = {
       ANSIBLE_FORCE_COLOR = "true"
-      ANSIBLE_HOST_KEY_CHECKING= "False"
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+      ANSIBLE_SSH_PASS = var.root_user_pass
     }
   }
 
